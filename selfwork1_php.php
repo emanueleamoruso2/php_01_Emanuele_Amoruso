abea7b896fcf7e09430512bb94c790c71bfeda8a
Emanuele Amoruso <emanueleamoruso33@outlook.com>
2026-04-21 20:29:11 +0200

    Esercizio1_completato

---
+++ selfwork1_php.php
@@ -0,0 +1,18 @@
+<?php
+$a = 4;
+$b = 4.5;
+$c = "stringa";
+$d = true;
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+const a = 4;
+const b = 4.5;
+const c = "stringa";
+const d = true ;
+var_dump(a);
+var_dump(b);
+var_dump(c);
+var_dump(d);
+ ?> 
